14576fef90d3b702e5bc80b91fffb328c242bf62
Ashu <ashu.kadipur12345@gmail.com>
2026-09-05 10:33:08 +0530

    hi cross2 commited

--- App.jsx
+++ App.jsx
@@ -3,6 +3,7 @@ const App=()=>{
         <View>
             <Text>hi this is new app data</Text>
             <Text>hi this is new app cross data</Text>
+            <Text>hi this is new app cross data</Text>
         </View>
     )
 }
